c3c05e60e6b42fb728d6a4a72eaf595aa37505a0
JohanMa4 <info@foreground.com.co>
2021-06-21 09:10:23 -0500

    add some views from profile and api

--- resources/js/Pages/API/Index.js
+++ resources/js/Pages/API/Index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import AppLayout from '../../Layouts/AppLayout';
 
-export default function Index() {
+export default function Index(props) {
+    const { tokens, availablePermissions, defaultPermissions } = props;
     return (
-        <div>
-            
-        </div>
+        <AppLayout>
+            <header>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    API Tokens
+                </h2>
+            </header>
+
+            <div>
+                <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                    <api-token-manager tokens={tokens}
+                        available-permissions={availablePermissions}
+                        default-permissions={defaultPermissions} />
+                </div>
+            </div>
+        </AppLayout>
     )
 }
--- resources/js/Pages/Auth/ConfirmPassword.js
+++ resources/js/Pages/Auth/ConfirmPassword.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import AuthenticationCard from '../../JetStream/AuthenticationCard';
+import AuthenticationCardLogo from '../../JetStream/AuthenticationCardLogo';
+import ValidationErrors from '../../JetStream/ValidationErrors';
+import Label from '../../JetStream/Label';
+import Input from '../../JetStream/Input';
+import Button from '../../JetStream/Button';
+import { useForm } from '@inertiajs/inertia-react';
 
 export default function ConfirmPassword() {
+    const { setData, post, processing, reset } = useForm({
+        password: ''
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('password.confirm'), {
+            onFinish: () => reset(),
+        })
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthenticationCard>
+            <section>
+                <AuthenticationCardLogo />
+            </section>
+
+            <div className="mb-4 text-sm text-gray-600">
+                This is a secure area of the application. Please confirm your password before continuing.
+            </div>
+
+            <ValidationErrors className='mb-4' />
+
+            <form onSubmit={submit}>
+                <div>
+                    <Label for="password" value="Password" />
+                    <Input id="password" type="password" className="mt-1 block w-full"
+                        required
+                        autoComplete="current-password" autoFocus
+                        onChange={e => setData('password', e.target.value)}
+                    />
+                </div>
+
+                <div className="flex justify-end mt-4">
+                    <Button className={"ml-4 " + processing ? 'opacity-25' : ''} disabled={processing}>
+                        Confirm
+                    </Button>
+                </div>
+            </form>
+        </AuthenticationCard>
     )
 }
