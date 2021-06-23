0aa384defe69d7173061cf6603d337019043df55
JohanMa4 <info@foreground.com.co>
2021-06-23 18:18:42 -0500

    add two views to react

--- resources/js/Pages/Auth/ResetPassword.js
+++ resources/js/Pages/Auth/ResetPassword.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import AuthenticationCard from '../../JetStream/AuthenticationCard';
+import AuthenticationCardLogo from '../../JetStream/AuthenticationCardLogo';
+import ValidationErrors from '../../JetStream/ValidationErrors';
 
-export default function ResetPassword() {
+export default function ResetPassword(props) {
+    const { token } = props;
+    const { data, setData, post, processing } = useForm({
+        token: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+    })
+    const submit = (e) => {
+        e.preventDefault();
+        post(this.route('password.update'), {
+            onFinish: () => reset('password', 'password_confirmation'),
+        })
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthenticationCard>
+            <section>
+                <AuthenticationCardLogo />
+            </section>
+
+            <ValidationErrors className="mb-4" />
+
+            <form onSubmit={submit}>
+                <div>
+                    <Label for="email" value="Email" />
+                    <Input id="email" type="email" className="mt-1 block w-full" required autoFocus />
+                </div>
+
+                <div className="mt-4">
+                    <Label for="password" value="Password" />
+                    <Input id="password" type="password" className="mt-1 block w-full" v-model="form.password" required autoComplete="new-password" />
+                </div>
+
+                <div className="mt-4">
+                    <Label for="password_confirmation" value="Confirm Password" />
+                    <Input id="password_confirmation" type="password" className="mt-1 block w-full" v-model="form.password_confirmation" required autoComplete="new-password" />
+                </div>
+
+                <div className="flex items-center justify-end mt-4">
+                    <Button className={processing && 'opacity-25'} disabled={processing}>
+                        Reset Password
+                    </Button>
+                </div>
+            </form>
+        </AuthenticationCard >
     )
 }
--- resources/js/Pages/Auth/VerifyEmail.js
+++ resources/js/Pages/Auth/VerifyEmail.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import AuthenticationCard from '../../JetStream/AuthenticationCard';
+import AuthenticationCardLogo from '../../JetStream/AuthenticationCardLogo';
+import Button from '../../JetStream/Button';
+import { InertiaLink } from '@inertiajs/inertia-react';
+import { useForm } from '@inertiajs/inertia-react';
 
-export default function VerifyEmail() {
+export default function VerifyEmail(props) {
+    const { status } = props;
+    const verificationLinkSent = status === 'verification-link-sent';
+    const { data, setData, post, processing, errors } = useForm();
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('verification.send'));
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthenticationCard>
+            <section>
+                <AuthenticationCardLogo />
+            </section>
+
+            <div class="mb-4 text-sm text-gray-600">
+                Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+            </div>
+
+            <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent" >
+                A new verification link has been sent to the email address you provided during registration.
+            </div>
+
+            <form onSubmit={submit}>
+                <div class="mt-4 flex items-center justify-between">
+                    <Button class="{'opacity-25': form.processing }" disabled="form.processing">
+                        Resend Verification Email
+                    </Button>
+
+                    <InertiaLink href={route('logout')} method="post" as="button" class="underline text-sm text-gray-600 hover:text-gray-900">Log Out</InertiaLink>
+                </div>
+            </form>
+        </AuthenticationCard>
     )
 }
